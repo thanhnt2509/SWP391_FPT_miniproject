64156fea3aa8e1e7c5972cf76ad352259371a278
thanhplassma <deathroot696@gmail.com>
2023-02-17 18:41:24 +0700

    fixed DAO

--- Bird_Accomodation_Vue_ExpressJs/API/src/modules/userModule.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/modules/userModule.js
@@ -14,7 +14,7 @@ async function addUser(bodyData) {
                 '${bodyData.status}', 
                 '${bodyData.user_img}', 
                 '${bodyData.token}')`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function updateUser(user_id, bodyData) {
@@ -27,34 +27,31 @@ async function updateUser(user_id, bodyData) {
                 email = '${bodyData.email}',
                 user_img = '${bodyData.user_img}'
                 WHERE user_id = '${user_id}'`
-    con.then(pool => pool.request().query(query))
+   return con.query(query);
 }
 
 async function deleteUser(user_id) {
     let con = await connection()
     let query = `update FROM [User] set status = 'ban' WHERE user_id = '${user_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function getUserById(user_id) {
     let con = await connection()
     let query = `SELECT * FROM [User] WHERE user_id = '${user_id}'`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getUserByName(Name) {
     let con = await connection()
     let query = `SELECT * FROM [User] WHERE Name = '${Name}'`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getAllUsers() {
     let con = await connection()
     let query = `SELECT * FROM [User]`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 // function 2
@@ -63,7 +60,7 @@ async function addBird(bodyData){
     let con = await connection()
     let query = `insert into Birds(user_id, bird_name, species, breed, age, gender, description)
                 values ('${bodyData.user_id}', '${bodyData.bird_name}', '${bodyData.species}', '${bodyData.breed}', '${bodyData.age}', '${bodyData.gender}', '${bodyData.description}')`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 
 }
 
@@ -74,35 +71,32 @@ async function updateBird(bird_id, bodyData){
                 age = '${bodyData.age}',
                 description = '${bodyData.description}'
                 WHERE bird_id = '${bird_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function deleteBird(bird_id){
     let con = await connection()
     let query = `DELETE FROM Birds WHERE bird_id = '${bird_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 
 }
 
 async function getBirdById(bird_id){
     let con = await connection()
     let query = `SELECT * FROM Birds WHERE bird_id = '${bird_id}'`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getBirdByName(bird_name){
     let con = await connection()
     let query = `SELECT * FROM Birds WHERE bird_name = '${bird_name}'`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getAllBirds(){
     let con = await connection()
     let query = `SELECT * FROM Birds`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 //function 3
@@ -110,7 +104,7 @@ async function addService(bodyData){
     //bodyData include name, description, price, status (1: active, 0: inactive)
     let con = await connection()
     let query = `INSERT INTO Services (name, description, price, status) VALUES ('${bodyData.name}', '${bodyData.description}', '${bodyData.price}', '${bodyData.status}')`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function updateService(service_id, bodyData){
@@ -121,34 +115,31 @@ async function updateService(service_id, bodyData){
                 price = '${bodyData.price}', 
                 status = '${bodyData.status}' 
                 WHERE service_id = '${service_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function deleteService(service_id){
     let con = await connection()
     let query = `DELETE FROM Services WHERE service_id = '${service_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function getServiceById(service_id){
     let con = await connection()
     let query = `SELECT * FROM Services WHERE service_id = '${service_id}'`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getServiceByName(name){
     let con = await connection()
     let query = `SELECT * FROM Services WHERE name = '${name}'`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getAllServices(){
     let con = await connection()
     let query = `SELECT * FROM Services`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 //function 4
@@ -157,7 +148,7 @@ async function addBooking(user_id, bodyData) {
     let con = await connection()
     let query = `INSERT INTO Bookings (user_id, date_from, date_to, total_price, status) 
                 VALUES ('${user_id}', '${bodyData.date_from}', '${bodyData.date_to}', '${bodyData.total_price}', '${bodyData.status}')`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function updateBooking(booking_id, bodyData) {
@@ -167,28 +158,26 @@ async function updateBooking(booking_id, bodyData) {
                 total_price = '${bodyData.total_price}', 
                 status = '${bodyData.status}'
                 WHERE booking_id = '${booking_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function cancelBooking(booking_id) {
     //status 1: pending, 2: approved, 3: cancelled
     let con = await connection()
     let query = `UPDATE Bookings SET status = 3 WHERE booking_id = '${booking_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function getBookingById(booking_id) {
     let con = await connection()
     let query = `SELECT * FROM Bookings WHERE booking_id = '${booking_id}'`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getAllBookings() {
     let con = await connection()
     let query = `SELECT * FROM Bookings`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 // function 5
@@ -197,7 +186,7 @@ async function addBookingDetail(booking_id, bodyData){
     let con = await connection()
     let query = `insert into BookingDetail (booking_id, bird_id, service_id) 
                 values ('${booking_id}', '${bodyData.bird_id}', '${bodyData.service_id}')`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function updateBookingDetail(bdetail_id, bodyData){
@@ -205,27 +194,27 @@ async function updateBookingDetail(bdetail_id, bodyData){
     let con = await connection()
     let query = `update BookingDetail set bird_id = '${bodyData.bird_id}', 
                 service_id = '${bodyData.service_id}' where bdetail_id = '${bdetail_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function deleteBookingDetail(bdetail_id){
     let con = connection()
     let query = `DELETE FROM BookingDetail WHERE bdetail_id = '${bdetail_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function getBookingDetailById(bdetail_id){
     let con = await connection()
     let query = `SELECT * FROM BookingDetail WHERE bdetail_id = '${bdetail_id}'`
     const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getAllBookingDetails(){
     let con = await connection()
     let query = `SELECT * FROM BookingDetail`
     const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 // function 6
@@ -234,7 +223,7 @@ async function addDailyReport(booking_id, bodyData){
     let con = await connection()
     let query = `insert into DailyReport (booking_id, bird_id, bird_image, date) 
                 values ('${booking_id}', '${bodyData.bird_id}', '${bodyData.bird_img}', '${bodyData.date}')`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 
 }
 
@@ -242,27 +231,25 @@ async function updateDailyReport(dreport_id, bodyData){
     //bodyData include bird_img
     let con = await connection()
     let query = `update DailyReport set bird_image = '${bodyData.bird_img}' where dreport_id = '${dreport_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function deleteDailyReport(dreport_id){
     let con = await connection()
     let query = `DELETE FROM DailyReport WHERE dreport_id = '${dreport_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function getDailyReportById(dreport_id){
     let con = await connection()
     let query = `SELECT * FROM DailyReport WHERE dreport_id = '${dreport_id}'`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getAllDailyReports(){
     let con = await connection()
     let query = `SELECT * FROM DailyReport`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 // function 7
@@ -271,34 +258,32 @@ async function addDailyFeedback(dreport_id, bodyData){
     let con = await connection()
     let query = `insert into DailyFeedback (title, content) 
                 values ('${bodyData.title}', '${bodyData.content}') where dreport_id = '${dreport_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function updateDailyFeedback(bodyData){
     let con = await connection()
     let query = `update DailyFeedback set title = '${bodyData.title}', 
                 content = '${bodyData.content}' where dfeedback_id = '${dfeedback_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function deleteDailyFeedback(){
     let con = await connection()
     let query = `DELETE FROM DailyFeedback WHERE dfeedback_id = '${dfeedback_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function getDailyFeedbackById(){
     let con = await connection()
     let query = `SELECT * FROM DailyFeedback WHERE dfeedback_id = '${dfeedback_id}'`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getAllDailyFeedbacks(){
     let con = await connection()
     let query = `SELECT * FROM DailyFeedback`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 // function 8
@@ -311,7 +296,7 @@ async function addBill(booking_id, bodyData) {
                     '${bodyData.status}', 
                     '${bodyData.method}', 
                     '${bodyData.date}')`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function updateBill(bill_id, bodyData) {
@@ -323,27 +308,25 @@ async function updateBill(bill_id, bodyData) {
             method = '${bodyData.method}',
             date = '${bodyData.date}' 
             WHERE bill_id = '${bill_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function deleteBill(bill_id) {
     let con = await connection()
     let query = `DELETE FROM Bills WHERE bill_id = '${bill_id}'`
-    con.then(pool => pool.request().query(query))
+    return con.query(query);
 }
 
 async function getBillById(bill_id) {
     let con = await connection()
     let query = `SELECT * FROM Bills WHERE bill_id = '${bill_id}'`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function getAllBills() {
     let con = await connection()
     let query = `SELECT * FROM Bills`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 // function 9
@@ -351,8 +334,7 @@ async function getAvgMonthIncome() {
     let con = await connection()
     let query = `SELECT MONTH(date) as month, AVG(total_amount) as avg_income 
                 FROM Bills GROUP BY MONTH(date)`
-    const data = con.then(pool => pool.request().query(query))
-    return (await data).recordset
+    return con.query(query);
 }
 
 async function addNews(dreport_id, bodyData) {
