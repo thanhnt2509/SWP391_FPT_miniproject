c75d519c4d2cabbeec337add6da7684042081635
thanhnt2509 <119075262+thanhnt2509@users.noreply.github.com>
2023-02-27 14:37:18 +0700

    optimize connection

--- Bird_Accomodation_Vue_ExpressJs/API/src/config/Connection.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/config/Connection.js
@@ -1,45 +1,28 @@
-const sql = require('mssql')
-const path = require('path');
-const envPath = path.resolve(__dirname, '..', '..', '.env');
-require('dotenv').config({ path: envPath })
-
-const config = {
-    server: process.env.SQL_SERVER,
-    database: process.env.SQL_DB,
-    user: process.env.SQL_USERNAME,
-    password: process.env.SQL_PASSWORD,
-    port: parseInt(process.env.SQL_PORT),
-    options: {
-        encrypt: false,
-        trustServerCertificate: true
+module.exports = {
+    sql: require('mssql'), // export mssql module
+    // dotenv and config will also be exported through this module, but most likely will not be used
+    dotenv: require('dotenv').config({ path: require('path').resolve(__dirname, '..', '..', '.env') }),
+    config: {
+        server: process.env.SQL_SERVER,
+        database: process.env.SQL_DB,
+        user: process.env.SQL_USERNAME,
+        password: process.env.SQL_PASSWORD,
+        port: parseInt(process.env.SQL_PORT),
+        options: {
+            encrypt: false,
+            trustServerCertificate: true
+        }
+    },
+    //export connection
+    connection: async () => {
+        try {
+            await module.exports.sql.connect(module.exports.config);
+            module.exports.sql.on('error', err => { console.log(err.message); }); //handle error
+            return module.exports.sql;
+        }
+        catch (err) {
+            console.log(err.message)
+        }
     }
 }
 
-// if error occur when import mssql module, please run this command in terminal
-// npm install mssql --save
-sql.on('error', err => { console.log(err.message); })
-/**
- * EX:
- *  const con = connection()
- *  con.then(pool => {
- *      const query = 'select * from Accounts'
- *      return pool.request().query(query)
- * }).then(data => {
- *      return data.recordSet
- * })
- *
- *
- * @returns Promise object provide request and query data
- */
-
-async function connection(){
-    try {
-        await sql.connect(config);
-        return sql;
-    }
-     catch (err) {
-         console.log(err.message)
-     }
-}
-
-module.exports = {connection}
