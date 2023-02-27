640b4c69f787f1e5a92a04d30fe8371de68d2709
thanhnt2509 <119075262+thanhnt2509@users.noreply.github.com>
2023-02-27 15:33:30 +0700

    update user

--- Bird_Accomodation_Vue_ExpressJs/API/src/controller/user.controller.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/controller/user.controller.js
@@ -1,10 +1,47 @@
-const userModel = require('../model/user.model');
+const userModel = require('../models/user.model');
 
 module.exports = {
     getUser: async (req, res, next) => {
-        
+        try {
+            const result = await userModel.getUser();
+            const userList = result.map(item => ({
+                user_id: item.user_id,
+                email: item.email,
+                name: item.name,
+                address: item.address,
+                phone: item.phone,
+                role: item.role,
+                status: item.status,
+                user_img: item.user_img,
+                token: item.token
+            }))
+            res.status(200).send({
+                exitcode: 0,
+                message: "Get user list successfully",
+                users: userList
+            })
+        } catch (error) {
+            next(error);
+        }
     },
     updateUserName: async (req, res, next) => {
-        
+        try {
+            const { user_id } = req.params;
+            const { name } = req.body;
+            const result = await userModel.updateUserName(user_id, name);
+            if (result > 0){
+                res.status(200).send({
+                    exitcode: 0,
+                    message: "Update user name successfully"
+                })
+            } else {
+                res.status(200).send({
+                    exitcode: 101,
+                    message: "Update user name failed"
+                })
+            } 
+        } catch (error) {
+            next(error);
+        }
     }
 }
--- Bird_Accomodation_Vue_ExpressJs/API/src/models/user.model.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/models/user.model.js
@@ -1,23 +1,21 @@
-const { connection } = require("../../src/config/Connection");
+const DBConnect = require("../../src/config/Connection");
 
 module.exports = {
     getUser: async () => {
-        try {
-            let con = await connection();
-            let sql = `SELECT * FROM [User]`;
-            return con.query(sql);
-        } catch (error) {
-            throw error;
-        }
+        let con = await DBConnect.connection();
+        const returnData = con.query("SELECT * FROM [User]");
+        return (await returnData).recordset || null;
     },
-    updateUserName: async (user_id, body) => {
-        try {
-            let con = await connection();
-            let sql = `UPDATE [User] SET name = '${body.name}' WHERE user_id = ${user_id}`;
-            return con.query(sql);
-        }catch (error) {
-            throw error;
-        }
+    updateUserName: async (user_id, name) => {
+        let con = await DBConnect.connection();
+        // let sql = `UPDATE [User] SET name = '${name}' WHERE user_id = ${user_id}`;
+        // const returnData = await con.query(sql);
+        const request = new con.Request();
+        const returnData = await request
+            .input("user_id", DBConnect.sql.Int, user_id)
+            .input("name", DBConnect.sql.NVarChar, name)
+            .query("UPDATE [User] SET name = @name WHERE user_id = @user_id");
+        return returnData.rowsAffected[0];
     }
 }
 
