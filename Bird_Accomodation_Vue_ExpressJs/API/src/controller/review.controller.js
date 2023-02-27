bdf9824492ce60c396afa28a1720de4592132668
thanhnt2509 <119075262+thanhnt2509@users.noreply.github.com>
2023-02-27 15:34:45 +0700

    minor update review, sussy

--- Bird_Accomodation_Vue_ExpressJs/API/src/controller/review.controller.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/controller/review.controller.js
@@ -1,7 +1,11 @@
-const reviewModel = require('../model/review.model');
+const reviewModel = require('../models/review.model');
 
 module.exports = {
     getAllReviews: async (req, res, next) => {
-        
+        try {
+            // code here
+        } catch (error) {
+            next(error);
+        }
     }
 }
