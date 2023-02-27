b1fbe1dcd002584b15855b517f038699882ff352
thanhnt2509 <119075262+thanhnt2509@users.noreply.github.com>
2023-02-27 15:32:48 +0700

    update post

--- Bird_Accomodation_Vue_ExpressJs/API/src/controller/posts.controller.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/controller/posts.controller.js
@@ -2,15 +2,89 @@ const postModel = require('../models/posts.model');
 
 module.exports = {
     getAllPosts: async (req, res, next) => {
-        
+        try {
+            const result = await postModel.getAllPosts();
+            const postList = result.map(item => ({
+                post_id: item.post_id,
+                title: item.title,
+                content: item.content,
+                reference: item.reference,
+            }))
+            res.status(200).send({
+                exitcode: 0,
+                message: "Get post list successfully",
+                posts: postList
+            })
+        } catch (error) {
+            next(error);
+        }
     },
     addPost: async (req, res, next) => {
-        
+        try {
+            const { title, content, reference } = req.body;
+            const postDetail = {
+                title: title,
+                content: content,
+                reference: reference
+            }
+            const result = await postModel.addPost(postDetail);
+            if (result > 0){
+                res.status(200).send({
+                    exitcode: 0,
+                    message: "Add post successfully"
+                })
+            } else {
+                res.status(200).send({
+                    exitcode: 101,
+                    message: "Add post failed"
+                })
+            }
+        } catch (error) {
+            next(error);
+        }
     },
     updatePostByID: async (req, res, next) => {
-        
+        try {
+            const { post_id } = req.params;
+            const { title, content, reference } = req.body;
+            const postDetail = {
+                title: title,
+                content: content,
+                reference: reference
+            }
+            const result = await postModel.updatePostByID(post_id, postDetail);
+            if (result > 0){
+                res.status(200).send({
+                    exitcode: 0,
+                    message: "Update post successfully"
+                })
+            } else {
+                res.status(200).send({
+                    exitcode: 101,
+                    message: "Update post failed"
+                })
+            }
+        } catch (error) {
+            next(error);
+        }
     },
     deletePostByID: async (req, res, next) => {
-        
+        try {
+            const { post_id } = req.params;
+            const result = await postModel.deletePostByID(post_id);
+            if (result > 0){
+                res.status(200).send({
+                    exitcode: 0,
+                    message: "Delete post successfully"
+                })
+            } else {
+                res.status(200).send({
+                    exitcode: 101,
+                    message: "Delete post failed"
+                })
+            }
+        } catch (error) {
+            next(error);
+        }
     },
 }
--- Bird_Accomodation_Vue_ExpressJs/API/src/models/posts.model.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/models/posts.model.js
@@ -1,44 +1,42 @@
-const { connection } = require("../../src/config/Connection");
+const { request } = require("express");
+const DBConnect = require("../../src/config/Connection");
 
 module.exports = {
     getAllPosts: async () => {
-        try {
-            let con = await connection();
-            let sql = `SELECT * FROM [Post]`;
-            return con.query(sql);
-        } catch (error) {
-            throw error;
-        }
+        let con = await DBConnect.connection();
+        let sql = `SELECT * FROM [Post]`;
+        const returnData = con.query(sql);
+        return (await returnData).recordset || null;
     },
     addPost: async (body) => {
         //body include: {title, content, reference}
-        try {
-            let con = await connection();
-            let sql = `INSERT INTO [Post] (title, content, reference) \n`
-                sql += `VALUES ('${body.title}', '${body.content}', '${body.reference}')`;
-            return con.query(sql);
-        } catch (error) {
-            throw error;
-        }
+        let con = await DBConnect.connection();
+        const request = new con.Request();
+        const returnData = await request
+            .input("title", DBConnect.sql.NVarChar, body.title)
+            .input("content", DBConnect.sql.NVarChar, body.content)
+            .input("reference", DBConnect.sql.NVarChar, body.reference)
+            .query("INSERT INTO [Post] (title, content, reference) VALUES (@title, @content, @reference)");
+        return returnData.rowsAffected[0];
     },
     updatePostByID: async (post_id, body) => {
-        try {
-            let con = await connection();
-            let sql = `UPDATE [Post] \n`
-                sql += `SET title = '${body.title}', content = '${body.content}', 
-                        reference = '${body.reference}' WHERE post_id = ${post_id}`;
-            return con.query(sql);
-        } catch (error) {
-            throw error;
-        }
+        //body include: {title, content, reference}
+        let con = await DBConnect.connection();
+        const request = new con.Request();
+        const returnData = await request
+            .input("post_id", DBConnect.sql.Int, post_id)
+            .input("title", DBConnect.sql.NVarChar, body.title)
+            .input("content", DBConnect.sql.NVarChar, body.content)
+            .input("reference", DBConnect.sql.NVarChar, body.reference)
+            .query("UPDATE [Post] SET title = @title, content = @content, reference = @reference WHERE post_id = @post_id");
+        return returnData.rowsAffected[0];
     },
     deletePostByID: async (post_id) => {
-        try {
-            let con = await connection();
-            let sql = `DELETE FROM [Post] WHERE post_id = ${post_id}`;
-            return con.query(sql);
-        } catch (error) {
-            throw error;
-        }
+        let con = await DBConnect.connection();
+        const request = new con.Request();
+        const returnData = await request
+            .input("post_id", DBConnect.sql.Int, post_id)
+            .query("DELETE FROM [Post] WHERE post_id = @post_id");
+        return returnData.rowsAffected[0];
     },
 }
