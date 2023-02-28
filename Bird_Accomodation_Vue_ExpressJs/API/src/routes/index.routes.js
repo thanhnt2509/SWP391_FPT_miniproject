26105b4705c19f407f63c2d10420aba8c92a3ad1
thanhnt2509 <119075262+thanhnt2509@users.noreply.github.com>
2023-03-01 00:21:20 +0700

    add manager routes to index

--- Bird_Accomodation_Vue_ExpressJs/API/src/routes/index.routes.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/routes/index.routes.js
@@ -4,6 +4,7 @@ const account = require('./account.routes');
 const posts = require('./posts.routes');
 const services = require('./services.routes');
 const reviews = require('./review.routes');
+const manager = require('./manager.routes');
 
 // router path
 const router = express.Router();
@@ -12,6 +13,7 @@ router.use('/account', account);
 router.use('/posts', posts);
 router.use('/services', services);
 router.use('/reviews', reviews);
+router.use('/manager', manager);
 
 
 
