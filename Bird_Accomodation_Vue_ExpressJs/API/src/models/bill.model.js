045710407af373fba9e2316d10c0c6a915412570
thanhnt2509 <119075262+thanhnt2509@users.noreply.github.com>
2023-03-08 22:54:53 +0700

    add bill model

---
+++ Bird_Accomodation_Vue_ExpressJs/API/src/models/bill.model.js
@@ -0,0 +1,30 @@
+const config = require('../config/config');
+
+module.exports = {
+    getBill: async (booking_id) => {
+        let con = await config.connection();
+        const request = new con.Request();
+        const returnData = await request
+            .input("booking_id", con.Int, booking_id)
+            .query("SELECT us.name, us.address, us.phone, br.bird_id, br.bird_name, bk.date_from, bk.date_to, bk.[status], b.total_amount \n" +
+                "FROM Bill AS b \n" +
+                "JOIN Booking AS bk ON b.booking_id = bk.booking_id \n" +
+                "JOIN [User] AS us ON bk.user_id = us.user_id \n" +
+                "JOIN Bird AS br ON bk.bird_id = br.bird_id \n" +
+                "WHERE bk.booking_id = @booking_id");
+        con.close();
+        return (await returnData).recordset || null;
+    },
+    getBillServiceDetail: async (booking_id) => {
+        let con = await config.connection();
+        const request = new con.Request();
+        const returnData = await request
+            .input("booking_id", con.Int, booking_id)
+            .query("SELECT s.service_id, s.name \n" +
+                "FROM BookingDetail AS bd \n" +
+                "JOIN Service AS s ON bd.service_id = s.service_id \n" +
+                "WHERE bd.booking_id = @booking_id");
+        con.close();
+        return (await returnData).recordset || null;
+    },
+}
