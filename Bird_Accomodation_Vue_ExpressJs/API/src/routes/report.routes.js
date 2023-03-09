fe4684fce4371ef98775aa75fee6b487fb18b6cd
thanhnt2509 <119075262+thanhnt2509@users.noreply.github.com>
2023-03-09 23:46:31 +0700

    add bill routes, minor update other routes

--- Bird_Accomodation_Vue_ExpressJs/API/src/routes/report.routes.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/routes/report.routes.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const report = require('../controller/report.controller')
+const { verifyAdmin } = require('../middlewares/verifyAdmin.mdw')
 
 
 const router = express.Router();
-
-router.route('/detail')
-    .patch(report.getReportDetail);
 
 // Need to change database 
 router.route('/')
     .get(report.getAllReport)
 
+router.route('/:booking_id')
+    .get(report.getReportByBookingId)
+
+router.route('/detail')
+    .get(report.getReportDetail);
+
 router.route('/newReport')
-    .post(report.addNewReport)
+    .post(verifyAdmin, report.addNewReport)
 
 
 module.exports = router;
