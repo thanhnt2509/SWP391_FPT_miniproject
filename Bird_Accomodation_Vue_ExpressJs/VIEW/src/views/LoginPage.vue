a50a6924f7536d447c41016adf1746018beffe3a
RaeKyoNeko <marchjeff@gmail.com>
2023-03-10 17:42:59 +0700

    update login, register page

--- Bird_Accomodation_Vue_ExpressJs/VIEW/src/views/LoginPage.vue
+++ Bird_Accomodation_Vue_ExpressJs/VIEW/src/views/LoginPage.vue
@@ -1,50 +1,54 @@
 <template>
-    <div class="container main">
-        <h1 class="title main_title has-text-centered">Login</h1>
-        <form @submit="submitForm">
-            <div v-if="isIncorrect">
-                <p style="color: red">Incorrect email or password</p>
-            </div>
-            <div class="field">
-                <p class="control has-icons-left has-icons-right">
-                    <input class="input" type="email" v-model="fields.email" placeholder="example@gmail.com" required>
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-envelope"></i>
-                    </span>
-                    <span class="icon is-small is-right">
-                        <i class="fas fa-check"></i>
-                    </span>
-                </p>
-            </div>
+    <div class="origin">
+        <div class="main">
+            <div class="form_input">
+                <h1 class="has-text-centered">Login</h1>
+                <form @submit="submitForm">
+                    <div v-if="isIncorrect">
+                        <p style="color: red">Incorrect email or password</p>
+                    </div>
+                    <div class="field">
+                        <p class="control has-icons-left has-icons-right">
+                            <input class="input" type="email" v-model="fields.email" placeholder="example@gmail.com"
+                                required>
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-envelope"></i>
+                            </span>
+                            <span class="icon is-small is-right">
+                                <i class="fas fa-check"></i>
+                            </span>
+                        </p>
+                    </div>
 
-            <div class="field">
-                <p class="control has-icons-left">
+                    <div class="field">
+                        <p class="control has-icons-left">
 
-                    <input class="input" v-model="fields.password" type="password" placeholder="Password" required>
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-lock"></i>
-                    </span>
-                </p>
+                            <input class="input" v-model="fields.password" type="password" placeholder="Password" required>
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-lock"></i>
+                            </span>
+                        </p>
+                    </div>
+
+                    <div>
+                        <label class="checkbox">
+                            <input v-model="fields.remember_me" type="checkbox">
+                            Remember me
+                        </label>
+                    </div>
+
+                    <div class="field">
+                        <p class="control">
+                            <button type="submit" class="button is-success">
+                                <div v-if="loading" class="ui text loader" />
+                                Login
+                            </button>
+                            or <router-link to="/register">Sign up</router-link>
+                        </p>
+                    </div>
+                </form>
             </div>
-
-            <div>
-                <label class="checkbox">
-                    <input v-model="fields.remember_me" type="checkbox">
-                    Remember me
-                </label>
-            </div>
-
-            <div class="field">
-                <p class="control">
-                    <button type="submit" class="button is-success">
-                        <div v-if="loading" class="ui text loader" />
-                        Login
-                    </button>
-                    or <router-link to="/register">Sign up</router-link>
-                </p>
-
-            </div>
-        </form>
+        </div>
     </div>
 </template>
 
@@ -81,6 +85,22 @@ export default {
 
 <style scoped>
 .main {
-    padding: 10%;
+    padding: 15% 7% 15% 61%;
+    margin: 0;
+}
+
+.origin {
+    background-image: url('../../public/images/wallpaper-bird.jpg');
+    background-position: left;
+    background-blend-mode: darken;
+    background-size: cover;
+}
+.form_input{
+    background-color: rgb(255, 255, 255, 0.2);
+    padding: 10px;
+    border-radius: 20px;
+}
+.form_input h1, p, label{
+    color: white;
 }
 </style>
--- Bird_Accomodation_Vue_ExpressJs/VIEW/src/views/RegisterPage.vue
+++ Bird_Accomodation_Vue_ExpressJs/VIEW/src/views/RegisterPage.vue
@@ -1,61 +1,64 @@
 <template>
-    <div class="container main">
-        <h1 class="title main_title has-text-centered">Register</h1>
-        <form @submit="submitForm">
-            <div class="field">
-                <label class="label">Fullname</label>
-                <div class="control">
-                    <input class="input is-success" type="text" placeholder="Text input">
-                </div>
-            </div>
+    <div class="origin">
+        <div class="main">
+            <div class="form_input">
+                <h1 class="title main_title has-text-centered">Register</h1>
+                <form @submit="submitForm">
+                    <div class="field">
+                        <label class="label">Fullname</label>
+                        <div class="control">
+                            <input class="input is-success" type="text" placeholder="Text input">
+                        </div>
+                    </div>
 
-            <div class="field">
-                <label class="label">Email</label>
-                <div class="control has-icons-left has-icons-right">
-                    <input class="input is-danger" type="email" placeholder="example@gmail.com">
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-envelope"></i>
-                    </span>
-                    <span class="icon is-small is-right">
-                        <i class="fas fa-exclamation-triangle"></i>
-                    </span>
-                </div>
-            </div>
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <div class="control has-icons-left has-icons-right">
+                            <input class="input is-danger" type="email" placeholder="example@gmail.com">
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-envelope"></i>
+                            </span>
+                            <span class="icon is-small is-right">
+                                <i class="fas fa-exclamation-triangle"></i>
+                            </span>
+                        </div>
+                    </div>
 
-            <div class="field">
-                <label class="label">Password</label>
-                <p class="control has-icons-left">
-                    <input class="input" type="password" placeholder="Password">
-                    <span class="icon is-small is-left">
-                        <i class="fas fa-lock"></i>
-                    </span>
-                </p>
-            </div>
+                    <div class="field">
+                        <label class="label">Password</label>
+                        <p class="control has-icons-left">
+                            <input class="input" type="password" placeholder="Password">
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-lock"></i>
+                            </span>
+                        </p>
+                    </div>
 
-            <div class="field">
-                <label class="label">Phone</label>
-                <div class="control">
-                    <input class="input is-success" type="text" placeholder="Phone">
-                </div>
-            </div>
+                    <div class="field">
+                        <label class="label">Phone</label>
+                        <div class="control">
+                            <input class="input is-success" type="text" placeholder="Phone">
+                        </div>
+                    </div>
 
-            <div class="field">
-                <label class="label">Address</label>
-                <div class="control">
-                    <input class="input is-success" type="text" placeholder="Phone">
-                </div>
-            </div>
+                    <div class="field">
+                        <label class="label">Address</label>
+                        <div class="control">
+                            <input class="input is-success" type="text" placeholder="Phone">
+                        </div>
+                    </div>
 
-            <input v-model="fields.remember_me" type="checkbox">
-            <div>
-                <input type="checkbox" v-model="term_and_condition">
-                <label>Term and conditions</label>
-            </div>
+                    <div>
+                        <input type="checkbox" v-model="term_and_condition">
+                        <label style="padding-left: 10px;">Term and conditions</label>
+                    </div>
 
-            <div>
-                <button class="button is-success" type="submit">Register</button>
+                    <div>
+                        <button class="button is-success" type="submit">Register</button>
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
 </template>
 <script>
@@ -89,9 +92,22 @@ export default {
 
 <style scoped>
 .main {
-    /* margin: 20px; */
-    padding: 10%;
-    /* border-radius: 40px; */
-    /* background-color: #f5f5f5; */
+    padding: 15% 7% 15% 61%;
+    margin: 0;
+}
+
+.origin {
+    background-image: url('../../public/images/wallpaper-bird.jpg');
+    background-position: left;
+    background-blend-mode: darken;
+    background-size: cover;
+}
+.form_input{
+    background-color: rgb(255, 255, 255, 0.2);
+    padding: 10px;
+    border-radius: 20px;
+}
+.form_input h1, p, label{
+    color: white;
 }
 </style>
