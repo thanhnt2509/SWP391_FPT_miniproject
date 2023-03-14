7115838b151ae8fe71c06498f0df22aa918fd2d2
RaeKyoNeko <marchjeff@gmail.com>
2023-03-14 08:46:37 +0700

    update profile page

--- Bird_Accomodation_Vue_ExpressJs/VIEW/src/components/modules/account/Profile.vue
+++ Bird_Accomodation_Vue_ExpressJs/VIEW/src/components/modules/account/Profile.vue
@@ -1,47 +1,28 @@
 <template>
-    <div class="container content main">
-        <h1 class="has-text-centered title">Profile</h1>
-        <p class="subtitle">Your information will show here, change it if you need to booking service in different address
-            !!!</p>
-        <button @click="edit" style="float: right;" class="button is-link">Edit</button>
-
-        <div class="field">
-            <label class="label">Fullname</label>
-            <div class="control">
-                <input class="input is-success" :disabled="!isEdit" type="text" v-model="fields.name">
+    <div class="wrapper container columns">
+        <div class="wrapper-info column is-8">
+            <h1 class="title">User information</h1>
+            <div class="field">
+                <label class="label">Name</label>
+                <input class="input" type="text" v-model="fields.name">
+            </div>
+            <div class="field">
+                <label class="label">Email</label>
+                <input disabled class="input" type="email" v-model="fields.email">
+            </div>
+            <div class="field">
+                <label class="label">Phone</label>
+                <input class="input" type="tel" v-model="fields.phone">
+            </div>
+            <div class="field">
+                <label class="label">Address</label>
+                <input class="input" type="text" v-model="fields.address">
             </div>
         </div>
-
-
-        <div class="field">
-            <label class="label">Email</label>
-            <div class="control has-icons-left has-icons-right">
-                <input class="input is-danger" :disabled="!isEdit" type="email" v-model="fields.email">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-envelope"></i>
-                </span>
-                <span class="icon is-small is-right">
-                    <i class="fas fa-exclamation-triangle"></i>
-                </span>
+        <div class="wrapper-avatar column is-4 ">
+            <div class="wrapper-avatar-img">
+                <img src="http://localhost:5000/file/get_img/avatar_user.jpg" alt="">
             </div>
-        </div>
-
-        <div class="field">
-            <label class="label">Phone</label>
-            <div class="control">
-                <input class="input is-success" :disabled="!isEdit" type="tel" v-model="fields.phone">
-            </div>
-        </div>
-
-        <div class="field">
-            <label class="label">Address</label>
-            <div class="control">
-                <input class="input is-success" :disabled="!isEdit" type="text" v-model="fields.address">
-            </div>
-        </div>
-
-        <div>
-            <button class="button is-primary" @click="submitForm">Submit</button>
         </div>
     </div>
 </template>
@@ -79,9 +60,40 @@ export default {
 </script>
 
 <style scoped>
-.main {
-    background-color: #f5f5f5;
-    padding: 10%;
-    border-radius: 40px;
+.wrapper {
+    width: 100%;
+    height: fit-content;
+    margin: 3% 13% 3% 17%;
+    /* background-color: rgb(223, 221, 221); */
+    border: 0.4px solid #aaaaaa;
+    border-radius: 20px;
+    overflow: hidden;
+}
+
+.wrapper-info {
+    background-color: #fff;
+    /* border-radius: 20px; */
+    padding: 20px;
+}
+
+.wrapper-avatar{
+
+}
+
+.wrapper-avatar-img{
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    border-radius: 20px;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.wrapper-avatar-img img{
+    width: 100%;
+    height: 100%;
+    object-fit: inherit;
+    border-radius: 20px;
 }
 </style>
