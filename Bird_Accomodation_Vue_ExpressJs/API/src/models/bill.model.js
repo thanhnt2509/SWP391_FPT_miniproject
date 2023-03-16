893a6b2f04c389688ef75e449b58525eec361a53
thanhnt2509 <119075262+thanhnt2509@users.noreply.github.com>
2023-03-16 08:49:06 +0700

    feat: knex migration to bill

--- Bird_Accomodation_Vue_ExpressJs/API/src/models/bill.model.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/models/bill.model.js
@@ -35,28 +35,19 @@ module.exports = {
         return returnData || null;
     },
     publishBill: async (booking_id) => {
-        let con = await config.connection();
-        const transaction = con.transaction();
-        await transaction.begin();
-        try {
-            await transaction.request()
-                .input('booking_id', sql.Int, booking_id)
-                .query(`INSERT INTO [Bill] (booking_id, total_amount, checkout_date) \n`
-                    + `VALUES (@booking_id, 0, GETDATE())`);
-            await transaction.commit();
-        } catch (err) {
-            console.log(err);
-            await transaction.rollback();
-        }
+        const returnData = await db('Bill')
+            .insert({
+                booking_id: booking_id,
+                total_amount: 0,
+                checkout_date: new Date()
+            });
+        return returnData || null;
     },
     getTotalAmountBooking_id: async (booking_id) => {
-        let con = await config.connection();
-        const request = con.request();
-        const returnData = await request
-            .input("booking_id", sql.Int, booking_id)
-            .query("SELECT SUM(booked_price) AS total_amount \n" +
-                "FROM BookingDetail \n" +
-                "WHERE booking_id = @booking_id");
-        return (await returnData).recordset || null;
+        const returnData = await db('BookingDetail AS bd')
+            .join('Service AS s', 'bd.service_id', 's.service_id')
+            .where('bd.booking_id', booking_id)
+            .sum('bd.booked_price AS total_amount');
+        return returnData || null;
     },
 }
