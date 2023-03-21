1231d81a4bdbb90cf2778612a2f7b570633d86a3
maotou-spy <phucbuihuu2604@gmail.com>
2023-03-21 15:45:26 +0700

    update function for Manage transaction and Manage Account

--- Bird_Accomodation_Vue_ExpressJs/API/src/controller/statistics.controller.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/controller/statistics.controller.js
@@ -1,17 +1,176 @@
-const statistics = require('../model/statistics.model')
+const statistics = require('../models/statistics.model')
 
 module.exports = {
     getTotalRevenueDay: async (req, res) => {
         try {
             const result = await statistics.getTotalRevenueDay();
-            if(result) {
+            if (result) {
                 res.status(200).json(result);
             } else {
-                res.status(404).json({message: 'Not found'});
+                res.status(404).json({ message: 'Not found' });
             }
-        }catch (e) {
+        } catch (e) {
             console.log(e)
-            res.status(500).json({message: 'Internal server error'});
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+    //GET the most used service
+    getTheMostUsedService: async (req, res) => {
+        try {
+            const result = await statistics.getTheMostUsedService();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+    //GET completion rate of quantity and remain
+    getCompletionRateOfQuantityAndRemain: async (req, res) => {
+        try {
+            const result = await statistics.getCompletionRateOfQuantityAndRemain();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+    //GET bird type frequently booked
+    getBirdTypeFrequentlyBooked: async (req, res) => {
+        try {
+            const result = await statistics.getBirdTypeFrequentlyBooked();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+    //GET  completion rate
+    getCompletionRate: async (req, res) => {
+        try {
+            const result = await statistics.getCompletionRate();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+
+    //Manage transaction
+    //GET number of booking in each status in this month
+    getNumberOfBookingInEachStatusInThisMonth: async (req, res) => {
+        try {
+            const result = await statistics.getNumberOfBookingInEachStatusInThisMonth();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+    //GET number of Bills in each payment method
+    getNumberOfBillsInEachPaymentMethod: async (req, res) => {
+        try {
+            const result = await statistics.getNumberOfBillInEachPaymentMethod();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+    //GET number of booking in each month in this year (base on date_from)
+    getNumberOfBookingsInEachMonth: async (req, res) => {
+        try {
+            const result = await statistics.getNumberOfBookingsInEachMonth();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+    //GET number of bird booked in this month
+    getNumberOfBirdBookedInThisMonth: async (req, res) => {
+        try {
+            const result = await statistics.getNumberOfBirdBookedInThisMonth();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+    //GET top 5 user who spend the most
+    getTop5UserWhoSpendTheMost: async (req, res) => {
+        try {
+            const result = await statistics.getTop5UserWhoSpendTheMost();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+
+    //Manage Account
+    //GET number of user in each role
+    getNumberOfUserInEachRole: async (req, res) => {
+        try {
+            const result = await statistics.getNumberOfUser();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
+        }
+    },
+    //GET number of user in each status
+    getNumberOfUserInEachStatus: async (req, res) => {
+        try {
+            const result = await statistics.getNumberOfUserInEachStatus();
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: 'Not found' });
+            }
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({ message: 'Internal server error' });
         }
     }
+
 }
--- Bird_Accomodation_Vue_ExpressJs/API/src/routes/statistics.routes.js
+++ Bird_Accomodation_Vue_ExpressJs/API/src/routes/statistics.routes.js
@@ -6,4 +6,40 @@ const router = express.Router()
 router.route("/total-revenue/day")
     .get(stat.getTotalRevenueDay)
 
+router.route("/most-used-service")
+    .get(stat.getTheMostUsedService)
+
+router.route("/completion-rate/quantity-remain")
+    .get(stat.getCompletionRateOfQuantityAndRemain)
+
+router.route("/bird-type-frequently-booked")
+    .get(stat.getBirdTypeFrequentlyBooked)
+
+router.route("/completion-rate")
+    .get(stat.getCompletionRate)
+
+//Mange transaction
+router.route("/transaction/number-booking-status")
+    .get(stat.getNumberOfBookingInEachStatusInThisMonth)
+
+router.route("/transaction/number-bill-status")
+    .get(stat.getNumberOfBillsInEachPaymentMethod)
+
+router.route("/transaction/number-booking-month")
+    .get(stat.getNumberOfBookingsInEachMonth)
+
+router.route("/transaction/number-bird-month")
+    .get(stat.getNumberOfBirdBookedInThisMonth)
+
+//Need more data
+// router.route("/transaction/top-5-user")
+//     .get(stat.getTop5UserWhoSpendTheMost)
+
+//Manage Account
+router.route("/account/number-account-status")
+    .get(stat.getNumberOfUserInEachStatus)
+
+router.route("/account/number-account-role")
+    .get(stat.getNumberOfUserInEachRole)
+
 module.exports = router
