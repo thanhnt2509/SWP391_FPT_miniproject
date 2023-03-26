b753f0d6c2c37bce6296e4fc3ec71ca66c34f623
RaeKyoNeko <marchjeff@gmail.com>
2023-03-26 13:52:47 +0700

    update mange Post, Account, Service view

--- Bird_Accomodation_Vue_ExpressJs/VIEW/src/components/modules/manager/Manage_Account.vue
+++ Bird_Accomodation_Vue_ExpressJs/VIEW/src/components/modules/manager/Manage_Account.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container content">
-        <h1 class="label has-text-centered">Manage Account</h1>
+    <div id="wrapper_manageAccount">
+        <h1>Manage Account</h1>
         <Manage_AccountList />
     </div>
 </template>
@@ -14,3 +14,21 @@ export default {
     }
 }
 </script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Popins', sans-serif;
+}
+
+#wrapper_manageAccount {
+  font-weight: 520;
+}
+
+#wrapper_manageAccount h1 {
+  text-align: center;
+  font-weight: 700;
+  font-size: 2.75rem;
+}
+</style>
--- Bird_Accomodation_Vue_ExpressJs/VIEW/src/components/modules/manager/Manage_Service.vue
+++ Bird_Accomodation_Vue_ExpressJs/VIEW/src/components/modules/manager/Manage_Service.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div id="wrapper_manageService">
     <h1>Manage Service</h1>
+
     <Manage_ServiceList />
   </div>
 </template>
@@ -16,4 +17,20 @@ export default {
 };
 </script>
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Popins', sans-serif;
+}
+
+#wrapper_manageService {
+  font-weight: 520;
+}
+
+#wrapper_manageService h1 {
+  text-align: center;
+  font-weight: 700;
+  font-size: 2.75rem;
+}
 </style>
